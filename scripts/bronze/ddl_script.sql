ceb244fe3ee1e4979617888c811311022a090f7c
pawancaliber <108858886+pawancaliber@users.noreply.github.com>
2025-11-18 23:56:37 +0530

    Update ddl_script.sql

--- scripts/bronze/ddl_script.sql
+++ scripts/bronze/ddl_script.sql
@@ -8,56 +8,70 @@ Script Purpose:
 	  Run this script to re-define the DDL structure of 'bronze' Tables
 ===============================================================================
 */
-	Truncate table bronze.crm_cst_info;
-	Bulk insert bronze.crm_cst_info
-	from 'D:\Study Material\SQL\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-	with(
-		Firstrow = 2,
-		Fieldterminator = ',',
-		tablock
-	);
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
 
-	Truncate table bronze.crm_prd_info;
-	Bulk insert bronze.crm_prd_info
-	from 'D:\Study Material\SQL\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-	with(
-		Firstrow = 2,
-		Fieldterminator = ',',
-		tablock
-	);
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
+);
 
-	Truncate Table bronze.crm_sales_details;
-	Bulk insert bronze.crm_sales_details
-	from 'D:\Study Material\SQL\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-	with(
-		Firstrow =2,
-		Fieldterminator = ',',
-		tablock
-	);
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
 
-	Truncate Table bronze.erp_cust_az12;
-	Bulk insert bronze.erp_cust_az12
-	from 'D:\Study Material\SQL\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-	with(
-		Firstrow = 2,
-		Fieldterminator = ',',
-		tablock
-	);
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
 
-	Truncate table bronze.erp_loc_a101;
-	Bulk insert bronze.erp_loc_a101
-	from 'D:\Study Material\SQL\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-	with(
-		Firstrow = 2,
-		Fieldterminator = ',',
-		tablock
-	);
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
 
-	Truncate Table bronze.erp_px_cat_g1v2;
-	Bulk insert bronze.erp_px_cat_g1v2
-	from 'D:\Study Material\SQL\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-	with(
-		Firstrow = 2,
-		Fieldterminator = ',',
-		tablock
-	);
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+
+CREATE TABLE bronze.erp_loc_a101 (
+    CID    NVARCHAR(50),
+    CNTRY  NVARCHAR(50)
+);
+
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+
+CREATE TABLE bronze.erp_cust_az12 (
+    CID   NVARCHAR(50),
+    BDATE  DATE,
+    GEN   NVARCHAR(50)
+);
+
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    ID          NVARCHAR(50),
+    CAT         NVARCHAR(50),
+    SUB       NVARCHAR(50),
+    MAINTENANCE  NVARCHAR(50)
+);
