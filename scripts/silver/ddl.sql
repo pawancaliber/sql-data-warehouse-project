2013eff8d0332526f4db86f111fe7f65d4ddbee7
pawancaliber <108858886+pawancaliber@users.noreply.github.com>
2025-11-20 02:38:03 +0530

    Update ddl.sql

--- scripts/silver/ddl.sql
+++ scripts/silver/ddl.sql
@@ -8,7 +8,8 @@ CREATE TABLE silver.crm_cust_info (
     cst_lastname        NVARCHAR(50),
     cst_marital_status  NVARCHAR(50),
     cst_gndr            NVARCHAR(50),
-    cst_create_date     DATE
+    cst_create_date     DATE,
+    dwh_create_date Datetime2 Default getdate()
 );
 
 IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
@@ -16,12 +17,13 @@ IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
 
 CREATE TABLE silver.crm_prd_info (
     prd_id       INT,
+    cat_id       NVARCHAR(50),
     prd_key      NVARCHAR(50),
     prd_nm       NVARCHAR(50),
     prd_cost     INT,
     prd_line     NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt   DATETIME
+    prd_start_dt Date,
+    prd_end_dt   Date
 );
 
 IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
@@ -31,12 +33,13 @@ CREATE TABLE silver.crm_sales_details (
     sls_ord_num  NVARCHAR(50),
     sls_prd_key  NVARCHAR(50),
     sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
+    sls_order_dt Date,
+    sls_ship_dt  Date,
+    sls_due_dt   Date,
     sls_sales    INT,
     sls_quantity INT,
-    sls_price    INT
+    sls_price    INT,
+    dwh_create_date Datetime2 Default getdate()
 );
 
 IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
@@ -44,7 +47,8 @@ IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
 
 CREATE TABLE silver.erp_loc_a101 (
     CID    NVARCHAR(50),
-    CNTRY  NVARCHAR(50)
+    CNTRY  NVARCHAR(50),
+    dwh_create_date Datetime2 Default getdate()
 );
 
 IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
@@ -53,7 +57,8 @@ IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
 CREATE TABLE silver.erp_cust_az12 (
     CID   NVARCHAR(50),
     BDATE  DATE,
-    GEN   NVARCHAR(50)
+    GEN   NVARCHAR(50),
+    dwh_create_date Datetime2 Default getdate()
 );
 
 IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
@@ -62,6 +67,7 @@ IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
 CREATE TABLE silver.erp_px_cat_g1v2 (
     ID          NVARCHAR(50),
     CAT         NVARCHAR(50),
-    SUB       NVARCHAR(50),
-    MAINTENANCE  NVARCHAR(50)
+    SUB      NVARCHAR(50),
+    MAINTENANCE  NVARCHAR(50),
+    dwh_create_date Datetime2 Default getdate()
 );
